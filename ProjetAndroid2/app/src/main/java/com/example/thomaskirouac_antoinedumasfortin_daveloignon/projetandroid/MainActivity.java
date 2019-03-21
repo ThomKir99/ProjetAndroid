82e48ab696902c3c9dcda9cd537a5e471d15c032
B533P-23\533 <abc>
2019-03-21 13:56:00 -0400

    template d'une photo avec un listener onClick()

--- ProjetAndroid2/app/src/main/java/com/example/thomaskirouac_antoinedumasfortin_daveloignon/projetandroid/MainActivity.java
+++ ProjetAndroid2/app/src/main/java/com/example/thomaskirouac_antoinedumasfortin_daveloignon/projetandroid/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.thomaskirouac_antoinedumasfortin_daveloignon.projetandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initListener();
+    }
+
+    private void initListener() {
+        ImageView imgFavorite = (ImageView) findViewById(R.id.imageView_futurama);
+        imgFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showOnMap();
+            }
+        });
+    }
+
+    private void showOnMap() {
+        Toast.makeText(MainActivity.this,
+                "The favorite list would appear on clicking this icon",
+                Toast.LENGTH_LONG).show();
     }
 }
